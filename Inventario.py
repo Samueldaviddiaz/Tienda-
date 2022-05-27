1fc93b2eb108fe2ea3463f4d495897474a94a65d
Samueldaviddiaz <101684426+Samueldaviddiaz@users.noreply.github.com>
2022-05-27 07:41:19 -0500

    Update Inventario.py

--- Inventario.py
+++ Inventario.py
@@ -1,18 +1,40 @@
+from Producto import Producto
+
 
 class Inventario:
-    def __init__(self, producto, cantidad):
-        self.producto = producto
-        self.cantidad = cantidad
+    def _init_(self):
+        self.inventario = []
 
-    def ingresar_producto(self):
-        pass
+    def existencias(self):
+        for i in self.inventario:
+            print(i.producto)
 
-    def eliminar_producto(self):
-        pass
+    def ingresar_producto(self, producto):
+        self.inventario.append(producto)
+        return "Producto añadido."
 
-    def consultar_producto(self):
-        pass
+    def eliminar_producto(self, producto):
+        cont = 0
+        for prod in self.inventario:
+            if prod.producto == producto:
+                self.inventario.pop(cont)
+            cont += 1
+        return "Producto eliminado."
 
-    def pedido_producto(self):
-        pass
-    
+    def consultar_prodcuto(self, producto):
+        for prod in self.inventario:
+            if prod.producto == producto:
+                return True
+        return False
+
+    def consultar_cantidad(self, producto):
+        for prod in self.inventario:
+            if prod.producto == producto:
+                return prod.cantidad
+        return 0
+
+    def pedido_producto(self, producto, cantidad):
+        if self.consultar_prodcuto(producto):
+            if cantidad <= self.consultar_cantidad(producto):
+                return "El pedido se puede realizar."
+        return "El pedido no se puede realizar."
